62858294c5f09475cc8eebd6f0b449e39ea9d7de
WangBo <66643052@qq.com>
2017-06-28 13:45:16 +0800

    add menuctrl.cs

---
+++ Assets/Script/UI/MenuCtrl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCtrl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowMenu() {
+        this.gameObject.SetActive(true);
+    }
+
+    public void BlankMenu()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
